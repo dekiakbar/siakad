e46593fb248796876874de18965fdea5cc516d9b
deki <dekiakbar@linuxmail.org>
2017-08-30 22:44:00 +0700

    tabel mahasiswa

---
+++ database/migrations/2017_08_30_154024_mahasiswa.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Mahasiswa extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mahasiswa', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nim');
+            $table->string('nama');
+            $table->string('alamat');
+            $table->string('jenis_kelamin');
+            $table->int('no_tlp');
+            $table->string('ttl');
+            $table->int('id_jurusan');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mahasiswa');
+    }
+}

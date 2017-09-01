80f94a3cbade1dd093571656ddc1bdf02369ccd3
deki <dekiakbar@linuxmail.org>
2017-09-02 05:47:20 +0700

    add matakuliah

---
+++ database/migrations/2017_09_02_054327_mata_kuliah.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class MataKuliah extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mata_kuliah', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('kode_mk');
+            $table->string('makul');
+            $table->string('sks');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mata_kuliah');
+    }
+}

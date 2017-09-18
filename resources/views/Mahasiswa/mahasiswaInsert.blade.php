956bbf2f0365716f0a4087c2a50c75e235357e61
deki <dekiakbar@linuxmail.org>
2017-09-18 09:00:57 +0700

    conver to blade form

--- resources/views/Mahasiswa/mahasiswaInsert.blade.php
+++ resources/views/Mahasiswa/mahasiswaInsert.blade.php
@@ -4,12 +4,92 @@
 
 @section('content')
 	<div class="col-sm-12 text-center">
-		<h3>Insert Data Mhasiswa</h3>
+		<h4>Insert Data Mahasiswa</h4>
 	</div>
-	<div class="container">
-		<form method="post" action="{{url('Mahasiswa')}}" enctype="multipart/form-data">
+	<div class="content">
+		{{csrf_field()}}
+		{{ Form::open(array('url' => 'Mahasiswa', 'method' => 'post','files' => true, 'class' => 'col s12')) }}
+			<div class="row">
+				<div class="col s6 input-field">
+					<i class="material-icons prefix">format_list_numbered</i>
+					{{ Form::text('nim',null,['id' => 'nim','data-length' => '8']) }}
+					<label for="nim">NIM</label>
+				</div>
+				<div class="input-field col s6">
+					<i class="material-icons prefix">account_circle</i>
+					{{ Form::text('nama',null,['id' => 'nama','data-length' => '30']) }}
+					<label for="nama">Nama Mahasiswa</label>
+				</div>
+			</div>
+			<div class="row">
+				<div class="input-field col s6">
+					<i class="material-icons prefix">phone</i>
+					{{ Form::text('no_tlp',null,['id' => 'notlp','data-length' => '12']) }}
+					<label for="notlp">No Telpon</label>
+				</div>
+				<div class="col s6 input-field">
+					<i class="material-icons prefix">email</i>
+					{{ Form::text('email',null,['id' => 'email','data-length' => '30']) }}
+					<label for="email">E-mail</label>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col s6 input-field">
+					<i class="material-icons prefix">assignment_ind</i>
+					{{ Form::text('tempat',null,['id' => 'tempat', 'data-length' => '20']) }}
+					<label for="tempat">Tempat Lahir</label>
+				</div>
+				<div class="col s6 input-field">
+					<i class="material-icons prefix">perm_contact_calendar</i>
+					{{ Form::text('tanggal',null,['id' => 'datepicker','data-length' => '15']) }}
+					<label for="datepicker">Tanggal Lahir</label>
+				</div>
+			</div>
+			<div class="row">
+				<div class="input-field col s6">
+					<div class="row">
+						<div class="col s4">
+							<i class="material-icons prefix">accessibility</i>
+							<label>Jenis Kelamin</label>
+						</div>
+						<div class="col s4">
+							{{ Form::radio('jenis_kelamin','Laki-Laki',true,['id' => 'jenis_kelamin','class' => 'with-gap']) }}
+							<label for="jenis_kelamin">Laki-Laki</label>
+						</div>
+						<div class="col s4">
+							{{ Form::radio('jenis_kelamin','Perempuan',false,['id' => 'jenis_kelamin1','class' => 'with-gap']) }}
+							<label for="jenis_kelamin1">Perempuan</label>
+						</div>
+					</div>
+				</div>
+				<div class="col s6 input-field">
+					<i class="material-icons prefix">photo_camera</i>
+					{{ Form::file('foto') }}
+				</div>
+			</div>
+			<div class="row">
+				<div class="col s6 input-field">
+					<i class="material-icons prefix">library_books</i>
+					{{ Form::textarea('alamat',null,['id' => 'alamat','data-length' => '100', 'class' => 'materialize-textarea']) }}
+					<label for="alamat">Alamat :</label>
+				</div>
+				<div class="col s6 input-field">
+					<i class="material-icons prefix">settings_applications</i>
+					{{ Form::select('id_jurusan',[
+						'1' => 'Teknik Informatika',
+						'2' => 'Teknik Sipil'],1,['id' => 'jurusan']
+					) }}
+					<label for="jurusan">Jurusan :</label>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col s12 text-center">
+					<button class="btn btn-sm waves-light waves-effect blue"><i class="material-icons right">send</i>Simpan</button>
+				</div>
+			</div>
+		{{ Form::close() }}
+		{{-- <form method="post" action="{{url('Mahasiswa')}}" enctype="multipart/form-data">
 			<div class="form-group row">
-				{{csrf_field()}}
 				<label class="col-sm-2 col-form-label col-form-label-lg">Nim :</label>
 				<div class="col-sm-10">
 					<input type="text" name="nim" class="form-control" placeholder="Masukan NIM">
@@ -72,7 +152,7 @@
 			<div class="col-sm-12 text-center">
 				<button type="submit" class="btn btn-primary ">Save</button>
 			</div>
-		</form>
+		</form> --}}
 		@if($errors->any())
 			<div class="col-sm-12">
     			<div class="alert alert-danger">

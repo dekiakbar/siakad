3205d5d8f77d0d589d252dfcc1a867b8ce4779f5
deki <dekiakbar@linuxmail.org>
2017-09-26 01:39:04 +0700

    layout insert krs

--- resources/views/Krs/krsInsert.blade.php
+++ resources/views/Krs/krsInsert.blade.php
@@ -9,11 +9,11 @@
 	<div class="content">
 		{{ Form::open(array('url' => 'Krs','class' => 'col s12', 'method' => 'post')) }}
 			<div class="row">
-				<div class="input-field col s4">
-					<div class="col s2">
+				<div class="input-field col s6">
+					<div class="col s3">
 						<label class="input-label">Nama :</label>
 					</div>
-					<div class="col s10">
+					<div class="col s9">
 						<select name="nim" style="display: inline-block;">
 							@foreach($mhs as $mhs)
 								<option value="{{ $mhs->nim }}">{{ $mhs->nama }} ({{ $mhs->nim }})</option>
@@ -21,30 +21,55 @@
 						</select>						
 					</div>
 				</div>
-				<div class="input-field col s5">
+				<div class="input-field col s6">
 					<div class="col s3">
 						<label class="input-label">Nama Dosen :</label>
 					</div>
-					<div class="col s7">
-						<select name="nip" style="display: inline-block;">
+					<div class="col s9">
+						<select name="kode_mk" style="display: inline-block;">
 							@foreach($dosen as $dosen)
 								<option value="{{ $dosen->nip }}">{{ $dosen->nama }} ({{ $dosen->nip }})</option>
 							@endforeach
 						</select>						
 					</div>
 				</div>
-				<div class="input-field col s5">
+			</div>
+			<div class="row">
+				<div class="input-field col s6">
 					<div class="col s3">
 						<label class="input-label">Mata Kuliah :</label>
 					</div>
-					<div class="col s7">
-						<select name="nip" style="display: inline-block;">
+					<div class="col s9">
+						<select name="makul" style="display: inline-block;">
 							@foreach($makul as $makul)
 								<option value="{{ $makul->kode_mk }}">{{ $makul->makul }} ({{ $makul->kode_mk }})</option>
 							@endforeach
 						</select>						
 					</div>
 				</div>
+				<div class="col s6">
+					<div class="col s12 input-field">
+						{{ Form::text('uts',null, array('id' => 'uts', 'data-length' => '3')) }}
+						<label for="uts">Masuka Nilai UTS</label>
+					</div>
+				</div>				
+			</div>
+			<div class="row">
+				<div class="col s6">
+					<div class="col s12 input-field">
+						{{ Form::text('uas',null, array('id' => 'uas', 'data-length' => '3')) }}
+						<label for="uas">Masuka Nilai UAS</label>
+					</div>
+				</div>
+				<div class="col s6">
+					<div class="col s12 input-field">
+						{{ Form::text('absen',null, array('id' => 'absen', 'data-length' => '3')) }}
+						<label for="absen">Masuka Nilai UTS</label>
+					</div>
+				</div>
+			</div>
+			<div class="col s12 text-center">
+				<button class="btn waves-effect waves-light blue"><i class="material-icons right">send</i>Simpan</button>
 			</div>
 		{{ Form::close() }}
 	</div>

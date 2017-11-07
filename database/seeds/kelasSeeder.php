e91448a9a9f26b7a39b195ebae22a4a00c349975
deki <dekiakbar@linuxmail.org>
2017-11-08 00:21:37 +0700

    update

--- database/seeds/kelasSeeder.php
+++ database/seeds/kelasSeeder.php
@@ -19,7 +19,7 @@ class kelasSeeder extends Seeder
         for ($i=0; $i < $limit ; $i++) { 
         	$jrs = Jurusan::find($faker->numberBetween($min=1,$max=9));
         	DB::table('kelas')->insert([
-        		'kode_kelas' => $faker->numerify('K####'),
+        		'kode_kelas' => $faker->unique()->numerify('K####'),
         		'nama_kelas' => $faker->numerify('C ##'),
         		'kode_jurusan' => $jrs->kode_jurusan,
         		'tahun' => $faker->date($format = 'Y-m-d', $max = 'now'),
